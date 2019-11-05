57b2ac550db60da1b9432fcf49d1d197616986df
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2019-11-05 15:11:48 +0300

    Add files via upload

---
+++ filters/elliptic1November.m
@@ -0,0 +1,61 @@
+function [e] = elliptic1November(f)
+[b,a] = ellip(3,5,50,[0.2 0.6],'stop');
+
+e = filter(b,a,f);
+
+[b,a] = ellip(3,5,50,[0.2 0.6],'stop');
+
+e = filter(b,a,e);
+
+plot(e)
+
+%{
+[b,a] = ellip(n,Rp,Rs,Wp,ftype)
+ n — Filter order
+integer scalar
+Filter order, specified as an integer scalar.
+
+Data Types: double
+
+Rp — Peak-to-peak passband ripple
+positive scalar
+Peak-to-peak passband ripple, specified as a positive scalar expressed in decibels.
+
+If your specification, ℓ, is in linear units, you can convert it to decibels using Rp = 40 log10((1+ℓ)/(1–ℓ)).
+
+Data Types: double
+
+Rs — Stopband attenuation
+positive scalar
+Stopband attenuation down from the peak passband value, specified as a positive scalar expressed in decibels.
+
+If your specification, ℓ, is in linear units, you can convert it to decibels using Rs = –20 log10ℓ.
+
+Data Types: double
+
+Wp — Passband edge frequency
+scalar | two-element vector
+Passband edge frequency, specified as a scalar or a two-element vector. The passband edge frequency is the frequency at which the magnitude response of the filter is –Rp decibels. Smaller values of passband ripple, Rp, and larger values of stopband attenuation, Rs, both result in wider transition bands.
+
+If Wp is a scalar, then ellip designs a lowpass or highpass filter with edge frequency Wp.
+
+If Wp is the two-element vector [w1 w2], where w1 < w2, then ellip designs a bandpass or bandstop filter with lower edge frequency w1 and higher edge frequency w2.
+
+For digital filters, the passband edge frequencies must lie between 0 and 1, where 1 corresponds to the Nyquist rate—half the sample rate or π rad/sample.
+
+For analog filters, the passband edge frequencies must be expressed in radians per second and can take on any positive value.
+
+Data Types: double
+
+ftype — Filter type
+'low' | 'bandpass' | 'high' | 'stop'
+Filter type, specified as one of the following:
+
+'low' specifies a lowpass filter with passband edge frequency Wp. 'low' is the default for scalar Wp.
+
+'high' specifies a highpass filter with passband edge frequency Wp.
+
+'bandpass' specifies a bandpass filter of order 2n if Wp is a two-element vector. 'bandpass' is the default when Wp has two elements.
+
+'stop' specifies a bandstop filter of order 2n if Wp is a two-element vector.
+%}
