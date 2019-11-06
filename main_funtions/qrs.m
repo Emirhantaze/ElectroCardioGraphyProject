a1f0d2b5aab9558a6096b9b777ffa283a9b6b61c
emtiyl <31549451+emtiyl@users.noreply.github.com>
2019-11-06 19:53:08 +0300

    Update qrs.m

--- main_funtions/qrs.m
+++ main_funtions/qrs.m
@@ -1,6 +1,6 @@
 function [sol] =  qrs(f,epeaks)
    sol=[];
-  a= mean(f)+30;
+  a= mean(f)+50;
    for i = 2:length(epeaks)-1
       
         if (abs(f(epeaks(i))-f(epeaks(i-1)))>a)||(abs(f(epeaks(i))-f(epeaks(i+1)))>a)
