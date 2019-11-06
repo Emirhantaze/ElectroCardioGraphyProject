1ba07d3d1a65da08c3458fd90704bb80f4f7da89
emtiyl <31549451+emtiyl@users.noreply.github.com>
2019-11-06 20:54:21 +0300

    bpm calculations are added

--- main_funtions/approach1.m
+++ main_funtions/approach1.m
@@ -1,5 +1,5 @@
 function [] = approach1(t,f,useperfect,selectedmod,lowfreq,highfreq)
-        
+ %%       
         [t,f]=findPerf(t,f,useperfect);
         [Fv ,Fecg]=myfft(t,f);
         Fecg(1)=0;
@@ -30,15 +30,51 @@ function [] = approach1(t,f,useperfect,selectedmod,lowfreq,highfreq)
         f=(f./max(f))*100;
         sol = qrs(f,peekfind(f));
         subplot(3,2,3)
-        title('Filtered Signal with peaks.')
+        
         plot(t,f);
         hold on;
         plot(t(sol(1:length(sol))),f(sol(1:length(sol))),"*r")
+        title(subplot(3,2,3),'Filtered Signal with peaks.')
         subplot(3,2,4)
-        title('Fft of filtered Signal.')
+        
         [Fv ,Fecg]=myfft(t,f);
         Fecg(1)=0;
         plot(Fv,Fecg);
-%         plot(signalparter(f,sol))
+        title(subplot(3,2,4),'Fft of filtered Signal.')
+%%        
+        subplot(3,1,3)
+        
+        
+        pt=islocalmax(f(sol(1:length(sol))));
+        p=[];
+       
+        for i = 1:length(pt)
+            
+            if pt(i)==true
+                p(i)=i;
+            end
+        end
+        i=1;
+        while i<=length(p)
+            if(p(i)==0)
+                p(i)=[];
+            else
+                i=i+1;
+            end
+        end
+        
+        bpm=[];
+        for i = 2:length(p)
+            bpm(i)=60/(t(sol(p(i)))-t(sol(p(i-1))));
+        end
+       bpm(1)=mean(bpm(2:length(bpm)));
+       
+       plot(t(sol(p)),bpm)
+       ylim([60 120])
+       a=mean(bpm);
+       legendmean=sprintf('Mean BPM: %.6f',a);
+       legend(legendmean)
+       grid on;
+%%       
 end
 
