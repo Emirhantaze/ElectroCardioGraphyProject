21b96d424a7922125c506cd7b8526a655b7f77ff
emtiyl <emtiyltezkry@gmail.com>
2019-11-11 21:42:41 +0300

    update

---
+++ python/ecgF.py
@@ -0,0 +1,16 @@
+import numpy as np
+import statistics
+def myfft(tin,fin):
+    ECG=fin
+    t=tin
+    L=len(t)
+    Ts = statistics.mean(np.diff(t))
+    Fs = 1/Ts                                     
+    Fn = Fs/2
+    FECG = np.fft.fft(ECG)
+    temp=np.floor(L/2)
+    Fv = np.linspace(0, 1, temp)*Fn
+    temp=len(Fv)+1
+    Iv = np.arange(1,temp)
+    FECG = np.abs(FECG[Iv])/L
+    return [Fv,FECG]
--- python/test.py
+++ python/test.py
@@ -1,1 +1,20 @@
-import meep as mp
+import time
+import ecgF as e
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io
+
+def main():
+    mat = scipy.io.loadmat('datas/31.10.2019.mat')
+    
+    t=mat['t9'][0]
+    f=mat['f9'][0]
+    f=-f
+    tf=e.myfft(t,f)
+    plt.plot(tf[0],tf[1])
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+    
