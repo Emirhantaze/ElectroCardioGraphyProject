05316a5b8b24e3f3408c55c93db4fcfbf1cb3838
emtiyl <31549451+emtiyl@users.noreply.github.com>
2019-11-11 17:38:04 +0300

    update

---
+++ livesignalrecording and processing/test.m
@@ -0,0 +1,4 @@
+tic
+[ttest,ftest] = findPerf(t9,-f9,true);
+plot(ttest,ftest)
+disp(toc)
--- main_funtions/findPerf.m
+++ main_funtions/findPerf.m
@@ -5,16 +5,16 @@ f=f-movmean(f,	75);        %dc value ve low freqanslara filtre at?yoruz
 temp=[];
 ii=0;
 if(flag)
-for i = 1:length(f)-1000
-    temp=[temp mean(diff(f(i:i+1000)))];
+for i = 1:length(f)-100
+    temp=[temp mean(diff(f(i:i+100)))];
     if abs(temp(i))==min(abs(temp))
         ii=i;
     end
 end
 % for içinde toplam bulunan türev de?erlerinin ortalamas?n? al?p en dü?ük
 % ortlalmal? yeri buluyor tabi istenirse istenmezse hiç çal??m?yor
-f=f(ii:ii+1000);
-t=t(ii:ii+1000);
+f=f(ii:ii+100);
+t=t(ii:ii+100);
 end
 end
 
