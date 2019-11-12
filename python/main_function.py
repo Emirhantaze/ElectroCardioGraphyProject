f9e311e4c83540092d197d1d920b3b8dc98ce622
emtiyl <emtiyltezkry@gmail.com>
2019-11-13 00:07:02 +0300

    1

--- python/main_function.py
+++ python/main_function.py
@@ -3,26 +3,24 @@ import ecgF as e
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
+from matplotlib.widgets import Cursor
 
 def main():
     mat = scipy.io.loadmat('datas/31.10.2019.mat')
     
     t=mat['t9'][0]
-    f=mat['f9'][0]
-    f=-f
+    f=-mat['f9'][0]
     print(len(f))
     tf=e.myfft(t,f)
-    plt.plot(tf[0],tf[1])
-    plt.xlim(0,100)
-    plt.grid(True)
-    plt.tick_params(direction='out', length=6, width=2, colors='r',
-               grid_color='r', grid_alpha=0.5,
-               grid_linestyle='-')
-    plt.ginput(n=-1,timeout=120)
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111)
+    ax.plot(tf[0],tf[1])
+    ax.set_xlim(0,100)
+    ax.grid(True)
+    cursor = Cursor(ax, useblit=True, color='r', linewidth=0.4)
     plt.show()
 
 
 
 if __name__ == "__main__":
-    main()
-    
+    main()    
