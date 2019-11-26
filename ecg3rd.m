ddacf4c9e4c6c8eb56d1bf28f6f84c8b16ebea09
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2019-11-26 11:10:59 +0300

    x

--- ecg3rd.m
+++ ecg3rd.m
@@ -1,20 +1,19 @@
 
 delete(instrfind);
-s = serial('COM3');
+s=serial('COM3');
 fopen(s);
 pause(.01);
 out=fscanf(s);
-
-
-i=0;
+i = 0;
 f20=0;
 t20=0;
-pause(3);  %this is test
+pause(3); %this is a test
 tic
-while toc<10
-i=i+1;
-out = fscanf(s);
-f20(i)=str2double(out);
-t20(i)=toc;
+while toc < 10
+    i= i +1;
+    out = fscanf(s);
+    f20(i)=str2double(out);
+    t20(i)=toc;
 end
 plot(t20,f20);
+
