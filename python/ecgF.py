f34cad39fe0d318813cfec2757b604d3b287bb5e
emirh <emirh@DESKTOP-ONKAPR8.local>
2019-11-29 19:09:12 +0300

    Update .DS_Store and ecgF.py

--- python/ecgF.py
+++ python/ecgF.py
@@ -1,11 +1,11 @@
+from __future__ import division
 import numpy as np
-import statistics
 def myfft(tin,fin):
     ECG=fin[np.arange(250,len(fin))]
     t=tin[np.arange(250,len(tin))]
     L=len(t)
-    Ts = statistics.mean(np.diff(t))
-    Fs = 1/Ts                                     
+    Ts = np.mean(np.diff(t))
+    Fs = 1/Ts                          
     Fn = Fs/2
     FECG = np.fft.fft(ECG)
     temp=np.floor(L/2)
@@ -14,3 +14,16 @@ def myfft(tin,fin):
     Iv = np.arange(1,temp)
     FECG = np.abs(FECG[Iv])/L
     return [Fv,FECG]
+def delete_first(t,f):
+    flag = True
+    i=0
+    difft = np.diff(t)
+    t=t[20:len(t)]
+    f=f[20:len(f)]
+    while flag:
+        if(difft(i)>0.006):
+            flag=False
+        i=i+1
+    t=t[i:len(t)]
+    f=f[i:len(f)]
+    return [t,f]
