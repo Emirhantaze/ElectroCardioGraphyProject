bf8d8694068b8b45a4e44ceada7eabe15b84d8c1
mucahitdemir <43001724+mucahitdemir@users.noreply.github.com>
2019-12-14 20:36:03 +0300

    Update main_function.py

--- python/main_function.py
+++ python/main_function.py
@@ -10,19 +10,20 @@ import talib as ta
 To use Move mean use ta.MA(FUNCTION,perioad)
 '''
 def main():
-    mat = scipy.io.loadmat('datas/31.10.2019.mat')
+    mat = scipy.io.loadmat('datas/31.10.2019.mat') #loading matlab file
     
-    t=mat['t9'][0]
+    t=mat['t9'][0] #What are the purpose of them?
     f=-mat['f9'][0]
     
     tf=e.myfft(t,f)
     fig = plt.figure(figsize=(8, 6))
-    plt.show(block=False)
+    plt.show(block=False) # what is that?
     ax = fig.add_subplot(111)
     ax.plot(tf[0],tf[1])
     ax.set_xlim(0,100)
     ax.grid(True)
-    plt.xticks(np.arange(0,101,5))
+    plt.xticks(np.arange(0,101,5)) #xtics : Get or set the current tick locations and labels of the x-axis.
+    #It is setting the x ticks with list of ticks.
     cursor = Cursor(ax, useblit=True, color='r', linewidth=0.4)
     plt.show(block=True)
 
